27eb9c82c08fafc7749e8c2ff4c53f082624fedb
Vanshgoel2610 <goelv2610@gmail.com>
2024-01-26 15:39:23 +0530

    last commit

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
 let myLeads = []
+// let myLeads = `["www.awesomelead.com"]`
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("www.lead2.com")
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
-    renderLeads()
-    
-    // To verify that it works:
-    console.log(localStorage.getItem("myLeads") )
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads)
 })
 
-function renderLeads() {
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = []
+    // localStorage.removeItem("myLeads")
+    localStorage.clear()
+    render(myLeads)
+})
+
+function render(leads) {
     // const li = document.createElement("li")
     // li.textContent = myLeads[i]
     // ulEl.append(li)
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+
+
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+// NaN
